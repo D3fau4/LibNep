8d1a4bbcb2fbe281539cf146d57acb2cb339aa64
D3fau4 <raulwii2@gmail.com>
2021-08-02 12:38:09 +0200

    Read SSA header

--- LibNep/FileFormats/SSA.cs
+++ LibNep/FileFormats/SSA.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibNep.FileFormats.SSAD;
 
 namespace LibNep.FileFormats
 {
@@ -12,28 +13,56 @@ namespace LibNep.FileFormats
     {
         static byte[] magic = { 0x53, 0x53, 0x41, 0x44 };
         DataReader reader;
+        int version;
+        int PartsCount;
+        int FPS;
+        PART[] parts;
 
         public SSA(string path)
         {
             var stream = DataStreamFactory.FromFile(path, FileOpenMode.Read);
             reader = new DataReader(stream);
-            throw new NotImplementedException();
+            load();
         }
         public SSA(Stream stream)
         {
             var stream2 = DataStreamFactory.FromStream(stream);
             reader = new DataReader(stream2);
-            throw new NotImplementedException();
+            load();
         }
         public SSA(DataStream stream)
         {
             reader = new DataReader(stream);
-            throw new NotImplementedException();
+            load();
         }
 
         internal void load()
         {
+            var origin = reader.Stream.Position;
 
+            // Read Magic
+            var _magic = reader.ReadBytes(4);
+            if (magic[0] != _magic[0] || magic[1] != _magic[1] || magic[2] != _magic[2] || magic[3] != _magic[3])
+                throw new Exception("Sistema de archivos no reconocido");
+
+            reader.Stream.Seek(0x4 + origin, SeekMode.Start);
+            version = reader.ReadInt32();
+
+            // Read the number of entrys/parts
+            reader.Stream.Seek(0x14 + origin);
+            PartsCount = reader.ReadInt32();
+            //parts = new PART[PartsCount]();
+            
+            // Frames per second (?)
+            reader.Stream.Seek(0x18 + origin);
+            FPS = reader.ReadInt32();
+            
+            // UNK bytes
+            reader.Stream.Seek(0x1C + origin);
+            var Unk1 = reader.ReadInt32();
+
+            // Read parts
+            PART[] parts = PART.GetPARTs(reader, PartsCount);
         }
     }
 }
---
+++ LibNep/FileFormats/SSAD/PART.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Yarhl.IO;
+
+namespace LibNep.FileFormats.SSAD
+{
+    public class PART
+    {
+        static public byte[] magic = { 0x50, 0x41, 0x52, 0x54 };
+        public int Version = 0;
+        public int Index = 0;
+        public string Name = "";
+        public int Sprite_positionX = 0;
+        public int Sprite_positionY = 0;
+
+        public static PART[] GetPARTs(DataReader reader, int numberofentrys)
+        {
+            PART[] parts = new PART[numberofentrys];
+            reader.Stream.Seek(0x20, SeekMode.Start);
+            var origin = reader.Stream.Position;
+            for (int i = 0; i < numberofentrys; i++)
+            {
+                reader.Stream.Seek(0x20, SeekMode.Start);
+                parts[i] = new PART();
+                // First PART
+                var _magic = reader.ReadBytes(4);
+                if (magic[0] != _magic[0] || magic[1] != _magic[1] || magic[2] != _magic[2] || magic[3] != _magic[3])
+                    throw new Exception("Incorrect file format");
+
+                parts[i].Version = reader.ReadInt32();
+                parts[i].Index = reader.ReadInt32();
+                // Skip NAME magic
+                reader.Stream.Seek(0x5, SeekMode.Current);
+                parts[i].Name = reader.ReadString(0x23);
+                
+                // Skip AREA magic
+                reader.Stream.Seek(0x4, SeekMode.Current);
+                var areaversion = reader.ReadInt32();
+                var areadata = reader.ReadBytes(0x10);
+
+                // Skip ORGX magic
+                reader.Stream.Seek(0x4, SeekMode.Current);
+                var unk = reader.ReadInt32();
+                parts[i].Sprite_positionX = reader.ReadInt32();
+
+                // Skip ORGY magic
+                reader.Stream.Seek(0x4, SeekMode.Current);
+                unk = reader.ReadInt32();
+                parts[i].Sprite_positionY = reader.ReadInt32();
+
+                // Skip TDBT magic
+                reader.Stream.Seek(0x4, SeekMode.Current);
+                unk = reader.ReadInt32();
+            }
+            return parts;
+        }
+    }
+}
